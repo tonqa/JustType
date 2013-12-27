88bd3e40ff2ec57782dd7a84742418b8cacad0fd
Andrea Koglin <Andrea@Andreas-MacBook.local>
2013-12-27 08:02:50 +0100

    added JustType header and JTKeyboardHeaders

---
+++ JustType/JTKeyboardHeaders.h
@@ -0,0 +1,13 @@
+//
+//  JTKeyboardHeaders.h
+//  JustType
+//
+//  Created by Andrea Koglin on 27.12.13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol JTKeyboardHeaders <NSObject>
+
+@end
---
+++ JustType/JustType.h
@@ -0,0 +1,16 @@
+//
+//  JustType.h
+//  JustType
+//
+//  Created by Andrea Koglin on 27.12.13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef __JustType__
+#define __JustType__
+
+#import "JTKeyboardListener.h"
+#import "JTTextField.h"
+#import "JTTextView.h"
+
+#endif
