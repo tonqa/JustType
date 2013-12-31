140eb1381aabd12a12e71ea283a777c7fa2c7d5d
Andrea Koglin <Andrea@Andreas-MacBook.local>
2013-12-31 14:06:32 +0100

    added custom gesture recognizer (which does not work in iOS5)

---
+++ JustType/JTKeyboardGestureRecognizer.m
@@ -0,0 +1,25 @@
+////
+////  JTKeyboardGestureRecognizer.m
+////  JustType
+////
+////  Created by Andrea Koglin on 31.12.13.
+////  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+////
+//
+//#import "JTKeyboardGestureRecognizer.h"
+//
+//@implementation JTKeyboardGestureRecognizer
+//
+//- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+//    [super touchesBegan:touches withEvent:event];
+//}
+//
+//- (void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+//    [super touchesMoved:touches withEvent:event];
+//}
+//
+//- (void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+//    [super touchesEnded:touches withEvent:event];
+//}
+//
+//@end
