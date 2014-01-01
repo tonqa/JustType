131f36a073ca9f7def5f1e9e4f51bd9526e3cc71
Alexander Koglin <tonqa@gmx.de>
2014-01-01 19:52:05 +0100

    quick fixed bug where initial selection did not work on iOS6

--- JustType/JTTextField.m
+++ JustType/JTTextField.m
@@ -129,7 +129,11 @@ UIKIT_STATIC_INLINE void mySelectionDidChange(id self, SEL _cmd, id<UITextInput>
 }
 
 - (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
-    [self.textController didChangeSelection];
+    double delayInSeconds = 0.2;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self.textController didChangeText];
+    });
     return YES;
 }
 
--- JustType/JTTextView.m
+++ JustType/JTTextView.m
@@ -106,8 +106,10 @@
 }
 
 - (BOOL)textViewShouldBeginEditing:(UITextView *)textView {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [self.textController didChangeSelection];
+    double delayInSeconds = 0.2;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [self.textController didChangeText];
     });
     return YES;
 }
