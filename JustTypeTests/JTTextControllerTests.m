0610693e0caf3127ab138cb6eaa91fc4e69992a7
Alexander Koglin <tonqa@gmx.de>
2014-01-07 08:13:51 +0100

    added some remaining tests

--- JustTypeTests/JTTextControllerTests.m
+++ JustTypeTests/JTTextControllerTests.m
@@ -10,10 +10,8 @@
 #import <OCMock/OCMock.h>
 #import "JTTextController.h"
 #import "JTTextController+TestsPrivate.h"
-
-@interface JTTestTextControllerDelegate : UITextView<UITextInput, JTTextControllerDelegate>
-@end
-
+#import "JTTextView.h"
+#import "JTSyntaxWord.h"
 
 @interface JTTextControllerTests : XCTestCase
 
@@ -38,9 +36,19 @@
     return [OCMockObject partialMockForObject:textController];
 }
 
-- (JTTestTextControllerDelegate *)mockedTextControllerDelegate {
-    JTTestTextControllerDelegate *textControllerDelegate = [[JTTestTextControllerDelegate alloc] init];
-    return [OCMockObject partialMockForObject:textControllerDelegate];
+- (JTTextView *)mockedTextControllerDelegate {
+    JTTextView *textControllerDelegate = [[JTTextView alloc] init];
+    textControllerDelegate = [OCMockObject partialMockForObject:textControllerDelegate];
+    [[[(id)textControllerDelegate stub] andReturnValue:[NSNumber numberWithBool:YES]] isFirstResponder];
+    [[[(id)textControllerDelegate stub] andReturn:[self mockedTextInputMode]] textInputMode];
+    return textControllerDelegate;
+}
+
+- (UITextInputMode *)mockedTextInputMode {
+    UITextInputMode *textInputMode = [OCMockObject mockForClass:[UITextInputMode class]];
+    
+    [[[(id)textInputMode stub] andReturn:@"en-US"] primaryLanguage];
+    return textInputMode;
 }
 
 - (void)testInitialSetup {
@@ -50,10 +58,10 @@
     XCTAssertTrue(textController.useSyntaxCompletion, @"syntax completion should be turned on initially");
 }
 
-- (void)testMoveToPreviousWord {
+- (void)testMoveToPreviousWordInBetweenText {
     // setup of the mock objects
     JTTextController *textController = [self mockedTextController];
-    JTTestTextControllerDelegate *testTextDelegate = [self mockedTextControllerDelegate];
+    JTTextView *testTextDelegate = [self mockedTextControllerDelegate];
     textController.delegate = testTextDelegate;
     
     // set the text and selection
@@ -61,33 +69,223 @@
     testTextDelegate.selectedRange = NSMakeRange(5, 0);
     
     // do the testing
+    [textController computeSyntaxWordWithForcedRecomputation:YES];
     [textController moveToPreviousWord];
     
     XCTAssertEqual(testTextDelegate.selectedRange.location, (NSUInteger)0, @"The selection should have been moved to position 0");
 }
 
-- (void)testMoveToNextWord {
+- (void)testMoveToPreviousWordAtBeginningOfText {
+    // setup of the mock objects
+    JTTextController *textController = [self mockedTextController];
+    JTTextView *testTextDelegate = [self mockedTextControllerDelegate];
+    textController.delegate = testTextDelegate;
     
+    // set the text and selection
+    testTextDelegate.text = @"This is a text";
+    testTextDelegate.selectedRange = NSMakeRange(0, 0);
+    
+    // do the testing
+    [textController computeSyntaxWordWithForcedRecomputation:YES];
+    [textController moveToPreviousWord];
+    
+    XCTAssertEqual(testTextDelegate.selectedRange.location, (NSUInteger)0, @"The selection should have been staying at position 0");
 }
 
-- (void)testMoveToPreviousLetter {
+- (void)testMoveToNextWordInBetweenTextAtBeginningOfWord {
+    // setup of the mock objects
+    JTTextController *textController = [self mockedTextController];
+    JTTextView *testTextDelegate = [self mockedTextControllerDelegate];
+    textController.delegate = testTextDelegate;
     
+    // set the text and selection
+    testTextDelegate.text = @"This is a text";
+    testTextDelegate.selectedRange = NSMakeRange(5, 0);
+    
+    // do the testing
+    [textController computeSyntaxWordWithForcedRecomputation:YES];
+    [textController moveToNextWord];
+    
+    XCTAssertEqual(testTextDelegate.selectedRange.location, (NSUInteger)7, @"The selection should have been moved to end of word at position 7");
+}
+
+- (void)testMoveToNextWordForCombinedWords {
+    // setup of the mock objects
+    JTTextController *textController = [self mockedTextController];
+    JTTextView *testTextDelegate = [self mockedTextControllerDelegate];
+    textController.delegate = testTextDelegate;
+    
+    // set the text and selection
+    testTextDelegate.text = @"With love; with joy.";
+    testTextDelegate.selectedRange = NSMakeRange(6, 0);
+    
+    // do the testing
+    [textController computeSyntaxWordWithForcedRecomputation:YES];
+    [textController moveToNextWord];
+    
+    XCTAssertEqual(testTextDelegate.selectedRange.location, (NSUInteger)9, @"The selection should have been moved between 'love' and ';' at position 9");
+}
+
+- (void)testMoveToPreviousLetterInBetweenText {
+    // setup of the mock objects
+    JTTextController *textController = [self mockedTextController];
+    JTTextView *testTextDelegate = [self mockedTextControllerDelegate];
+    textController.delegate = testTextDelegate;
+    
+    // set the text and selection
+    testTextDelegate.text = @"With love; with joy.";
+    testTextDelegate.selectedRange = NSMakeRange(6, 0);
+    
+    // do the testing
+    [textController computeSyntaxWordWithForcedRecomputation:YES];
+    [textController moveToPreviousLetter];
+    
+    XCTAssertEqual(testTextDelegate.selectedRange.location, (NSUInteger)5, @"The selection should have been moved one letter to the left");
+}
+
+- (void)testMoveToPreviousLetterAtBeginningOfText {
+    // setup of the mock objects
+    JTTextController *textController = [self mockedTextController];
+    JTTextView *testTextDelegate = [self mockedTextControllerDelegate];
+    textController.delegate = testTextDelegate;
+    
+    // set the text and selection
+    testTextDelegate.text = @"With love; with joy.";
+    testTextDelegate.selectedRange = NSMakeRange(0, 0);
+    
+    // do the testing
+    [textController computeSyntaxWordWithForcedRecomputation:YES];
+    [textController moveToPreviousLetter];
+    
+    XCTAssertEqual(testTextDelegate.selectedRange.location, (NSUInteger)0, @"The selection should have stayed at position 0");
 }
 
 - (void)testMoveToNextLetter {
+    // setup of the mock objects
+    JTTextController *textController = [self mockedTextController];
+    JTTextView *testTextDelegate = [self mockedTextControllerDelegate];
+    textController.delegate = testTextDelegate;
     
+    // set the text and selection
+    testTextDelegate.text = @"This is a test.";
+    testTextDelegate.selectedRange = NSMakeRange(6, 0);
+    
+    // do the testing
+    [textController computeSyntaxWordWithForcedRecomputation:YES];
+    [textController moveToNextLetter];
+    
+    XCTAssertEqual(testTextDelegate.selectedRange.location, (NSUInteger)7, @"The selection should have moved one to the right");
 }
 
-- (void)testSelectPreviousSuggestion {
+- (void)testAddingSpaceAtEndOfTextWhenMovingToNextWord {
+    // setup of the mock objects
+    JTTextController *textController = [self mockedTextController];
+    JTTextView *testTextDelegate = [self mockedTextControllerDelegate];
+    textController.delegate = testTextDelegate;
     
+    // set the text and selection
+    testTextDelegate.text = @"This is a test";
+    testTextDelegate.selectedRange = NSMakeRange(testTextDelegate.text.length, 0);
+    
+    // do the testing
+    [textController computeSyntaxWordWithForcedRecomputation:YES];
+    [textController moveToNextWord];
+    
+    XCTAssertEqualObjects(testTextDelegate.text, @"This is a test ", @"space should have been added to the text");
+    XCTAssertEqual(testTextDelegate.selectedRange.location, testTextDelegate.text.length, @"The selection should be at the end");
+}
+
+- (void)testAddingDotAtEndOfTextWhenMovingToNextWord {
+    // setup of the mock objects
+    JTTextController *textController = [self mockedTextController];
+    JTTextView *testTextDelegate = [self mockedTextControllerDelegate];
+    textController.delegate = testTextDelegate;
+    
+    // set the text and selection
+    testTextDelegate.text = @"This is a test ";
+    testTextDelegate.selectedRange = NSMakeRange(testTextDelegate.text.length, 0);
+    
+    // do the testing
+    [textController computeSyntaxWordWithForcedRecomputation:YES];
+    [textController moveToNextWord];
+    
+    XCTAssertEqualObjects(testTextDelegate.text, @"This is a test. ", @"space should have been added to the text");
+    XCTAssertEqual(testTextDelegate.selectedRange.location, testTextDelegate.text.length, @"The selection should be at the end");
 }
 
 - (void)testSelectNextSuggestion {
+    // setup of the mock objects
+    JTTextController *textController = [self mockedTextController];
+    JTTextView *testTextDelegate = [self mockedTextControllerDelegate];
+    textController.delegate = testTextDelegate;
     
+    // set the text and selection
+    testTextDelegate.text = @"This is a test ";
+    testTextDelegate.selectedRange = NSMakeRange(0, 0);
+    
+    // do the testing
+    [textController computeSyntaxWordWithForcedRecomputation:YES];
+
+    id<JTSyntaxWord> syntaxWord = [textController selectedSyntaxWord];
+    XCTAssertEqualObjects(syntaxWord.text, @"This", @"The right syntax word should have been selected");
+    XCTAssertEqual([textController selectedSyntaxWordRange].location, (NSUInteger)0, @"The selected word range should begin with 0");
+    XCTAssertEqual([textController selectedSyntaxWordRange].length, [syntaxWord.text length], @"The selected word range should have the text length");
+
+    [textController selectNextSuggestion];
+    
+    NSString *firstSuggestion = [[syntaxWord allSuggestions] objectAtIndex:0];
+    XCTAssertTrue([testTextDelegate.text hasPrefix:firstSuggestion], @"The text should begin with the first suggestion now");
+    XCTAssertEqual(testTextDelegate.selectedRange.location, firstSuggestion.length, @"The selection should be at the end");
+    XCTAssertEqual([textController selectedSyntaxWordRange].location, (NSUInteger)0, @"The selected word range should begin with 0");
+    XCTAssertEqual([textController selectedSyntaxWordRange].length, [firstSuggestion length], @"The selected word range should have the next suggestion text length");
+}
+
+- (void)testSelectPreviousSuggestion {
+    // setup of the mock objects
+    JTTextController *textController = [self mockedTextController];
+    JTTextView *testTextDelegate = [self mockedTextControllerDelegate];
+    textController.delegate = testTextDelegate;
+    
+    // set the text and selection
+    testTextDelegate.text = @"This is a test ";
+    testTextDelegate.selectedRange = NSMakeRange(0, 0);
+    
+    // do the testing
+    [textController computeSyntaxWordWithForcedRecomputation:YES];    
+    id<JTSyntaxWord> syntaxWord = [textController selectedSyntaxWord];
+    
+    [textController selectPreviousSuggestion];
+    
+    NSString *previousSuggestion = [[syntaxWord allSuggestions] objectAtIndex:[[syntaxWord allSuggestions] count]-1];
+    XCTAssertTrue([testTextDelegate.text hasPrefix:previousSuggestion], @"The text should begin with the previous suggestion now");
+    XCTAssertEqual(testTextDelegate.selectedRange.location, previousSuggestion.length, @"The selection should be at the end of the word");
+    XCTAssertEqual([textController selectedSyntaxWordRange].location, (NSUInteger)0, @"The selected word range should begin with 0");
+    XCTAssertEqual([textController selectedSyntaxWordRange].length, [previousSuggestion length], @"The selected word range should have the previous suggestion text length");
 }
 
 - (void)testSelectSuggestionByIndex {
+    // setup of the mock objects
+    JTTextController *textController = [self mockedTextController];
+    JTTextView *testTextDelegate = [self mockedTextControllerDelegate];
+    textController.delegate = testTextDelegate;
     
+    // set the text and selection
+    testTextDelegate.text = @"This is a test ";
+    testTextDelegate.selectedRange = NSMakeRange(0, 0);
+    
+    // do the testing
+    [textController computeSyntaxWordWithForcedRecomputation:YES];
+    id<JTSyntaxWord> syntaxWord = [textController selectedSyntaxWord];
+    
+    // we get a random selected index via modulo (to be safe)
+    NSUInteger selectedIndex = 3 % [[syntaxWord allSuggestions] count];
+    NSString *selectedSuggestion = [[syntaxWord allSuggestions] objectAtIndex:selectedIndex];
+    [textController selectSuggestionByIndex:selectedIndex];
+    
+    XCTAssertTrue([testTextDelegate.text hasPrefix:selectedSuggestion], @"The text should begin with the selected suggestion now");
+    XCTAssertEqual(testTextDelegate.selectedRange.location, selectedSuggestion.length, @"The selection should be at the end of the word");
+    XCTAssertEqual([textController selectedSyntaxWordRange].location, (NSUInteger)0, @"The selected word range should begin with 0");
+    XCTAssertEqual([textController selectedSyntaxWordRange].length, [selectedSuggestion length], @"The selected word range should have the selected suggestion text length");
 }
 
 @end
