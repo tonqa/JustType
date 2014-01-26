07f1257d77d9b17804a40a969d5ec16f6aad66c7
Alexander Koglin <tonqa@gmx.de>
2014-01-26 10:13:02 +0100

    adapted the dashed pattern to be more subtle

--- JustType/JTDashedBorderedView.m
+++ JustType/JTDashedBorderedView.m
@@ -22,28 +22,27 @@
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
-    CGFloat dashPattern[]= {5.0, 2};
+    CGFloat dashPattern[]= {5.0, 1};
     
     CGContextRef context = UIGraphicsGetCurrentContext();
     CGContextSetRGBStrokeColor(context, 0.0, 0.0, 1.0, 1.0);
 
-    CGPoint fromPoint = CGPointMake(self.bounds.origin.x + 2,
+    CGPoint fromPoint = CGPointMake(self.bounds.origin.x,
                                     self.bounds.origin.y +
-                                    self.bounds.size.height - 4);
+                                    self.bounds.size.height);
+    
     CGPoint toPoint = CGPointMake(self.bounds.origin.x +
-                                  self.bounds.size.width + 2,
+                                  self.bounds.size.width,
                                   self.bounds.origin.y +
-                                  self.bounds.size.height - 4);
+                                  self.bounds.size.height);
     
     CGContextSetLineWidth(context, 2.0);
     CGContextSetLineDash(context, 0.0, dashPattern, 2);
     
     CGContextMoveToPoint(context, fromPoint.x, fromPoint.y);
     CGContextAddLineToPoint(context, toPoint.x, toPoint.y);
-    //CGContextAddRect(context, self.bounds);
     
     CGContextStrokePath(context);
-    //CGContextClosePath(context);
     CGContextDrawPath(context, kCGPathStroke);
 }
 
